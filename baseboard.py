2e1a145681ce3ca43bb688314548e8db9df25b3c
Angellou <Angellou_Svj@outlook.com>
2024-09-14 12:48:42 -0700

    implemented updater on pi

--- baseboard.py
+++ baseboard.py
@@ -3,6 +3,10 @@ from PIL import Image, ImageTk
 from welcome_screen import WelcomeScreen
 # from pin_entry_page import PinEntryPage as WelcomeScreen
 from PairCard import PairCardPage
+import websockets
+import os
+import json
+import asyncio
 class Baseboard(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -11,6 +15,7 @@ class Baseboard(tk.Tk):
         self.attributes("-fullscreen", True)
         self.attributes("-type", "splash")
         self.current_frame = None
+        asyncio.run(self.updater())
         self.show_screen(WelcomeScreen)
         # self.show_screen(PairCardPage)
 
@@ -28,6 +33,27 @@ class Baseboard(tk.Tk):
         self.current_frame = new_frame
         self.current_frame.pack()
 
+    async def updater(self):
+        async with websockets.connect("ws://73.157.88.153:8000/wss") as websocket:
+            print("HI WSS")
+            
+            try:
+                commit_hash = os.popen('git rev-parse HEAD').read().strip()
+                data = {
+                        "cmd": "insert_hash", 
+                        "piHash": commit_hash
+                }
+                await websocket.send(json.dumps(data))
+                print("DONE!!!")
+            except Exception as e:
+                print(f"Error sending confirmation: {e}")
+        return
+
+
+
 if __name__ == "__main__":
     app = Baseboard()
     app.mainloop()
+
+
+

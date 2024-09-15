c6b1183f7cbb01d2d27500d3638e96542efbba83
VP1606 @ 16MBP <vpremakantha@gmail.com>
2024-09-16 00:49:12 +0100

    New attempt.

--- baseboard.py
+++ baseboard.py
@@ -10,6 +10,13 @@ import asyncio
 import threading
 import os
 
+from final_page import FinalPage
+
+from mfrc522 import SimpleMFRC522
+import RPi.GPIO as GPIO
+
+import mysql.connector
+
 class Baseboard(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -19,7 +26,10 @@ class Baseboard(tk.Tk):
         self.attributes("-type", "splash")
         self.current_frame = None
         
+        self.current_screen = None
+        
         self.start_updater_watcher()
+        self.start_background_scanning()
         
         self.show_screen(WelcomeScreen)
         # self.show_screen(PairCardPage)
@@ -31,6 +41,7 @@ class Baseboard(tk.Tk):
         thread.start()
 
     def show_screen(self, screen_class, *args, **kwargs):
+        self.current_screen = screen_class
         new_frame = screen_class(self, *args, **kwargs)
         if self.current_frame is not None:
             self.current_frame.destroy()
@@ -43,6 +54,72 @@ class Baseboard(tk.Tk):
             self.current_frame.destroy()
         self.current_frame = new_frame
         self.current_frame.pack()
+        
+    def start_background_scanning(self):
+        # Create and start a thread to run the general_scan main function
+        thread = threading.Thread(target=asyncio.run, args=(self.general_scan(),))
+        thread.daemon = True  # This ensures the thread will close when the main program exits
+        thread.start()
+        
+    async def general_scan(self):
+        reader = SimpleMFRC522()
+        try:
+            while True:
+                print("Hold a tag near the reader")
+                id, text = reader.read()
+                # id = "523"
+                print(f"ID: {id}")
+                
+                if self.current_screen == WelcomeScreen:
+                    self.welcome_screen_scan_handler(id)
+                    
+                GPIO.cleanup()
+                reader.read_no_block()
+                
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            GPIO.cleanup()   
+             
+        print("Bye Bye")
+    
+    async def welcome_screen_scan_handler(self, id):
+        async with websockets.connect("ws://73.157.88.153:8000/wss") as websocket:
+            try:
+                data = {
+                    "identityCode": id,
+                    "cmd": "user_scanned"
+                }
+                try:
+                    await websocket.send(json.dumps(data))
+                except (websocket.WebSocketConnectionClosedException, BrokenPipeError):
+                    print("Connection lost, reconnecting...")
+                    # ws = connect_websocket()
+                    await websocket.send(json.dumps(data))
+                    
+                mydb = mysql.connector.connect(
+                    host="73.157.88.153",
+                    user="piuser",
+                    password="password",
+                    database="venitian"
+                )
+        
+                mycursor = mydb.cursor()
+                sql = f"SELECT * FROM employees WHERE identity_code = {id}"
+                mycursor.execute(sql)
+                
+                results = mycursor.fetchall()
+                if len(results) == 1:
+                    print("VALID PIN")
+                    print(results)
+                    mydb.close()
+                    
+                    self.master.show_screen(FinalPage, name=results[0][1])
+                    return
+                else:
+                    print("INVALID PIN")
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                await websocket.close()
 
     async def updater(self):
         while True:

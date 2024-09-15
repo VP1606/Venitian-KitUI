361a96e1ec132a3f1669ceff97499eba3c3e6955
VP1606 @ 16MBP <vpremakantha@gmail.com>
2024-09-15 18:57:55 +0100

    Added test updater script.

--- update-kit.sh
+++ update-kit.sh
@@ -4,17 +4,20 @@
 echo "HELLO from Kit-UPDATER"
 echo " "
 
-# Quit UI Instance.
-echo "Quitting UI..."
-echo "Done."
-echo " "
+# # Quit UI Instance.
+# echo "Quitting UI..."
+# echo "Done."
+# echo " "
 
 # Pull from Git / update files.
 echo "Pulling from Git..."
+git pull
 echo "Done."
 echo " "
-# Restart UI Instance.
+
+# # Restart UI Instance.
 echo "Restarting UI..."
+sudo systemctl restart venitian_ui.service
 echo "Done."
 echo " "
 

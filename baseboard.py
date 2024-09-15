8ae910d53e7bef3654fe3302a7accb3e7267699d
VP1606 @ 16MBP <vpremakantha@gmail.com>
2024-09-15 17:56:40 +0100

    Added websocket reciever as a background thread.

--- baseboard.py
+++ baseboard.py
@@ -7,6 +7,8 @@ import websockets
 import os
 import json
 import asyncio
+import threading
+
 class Baseboard(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -15,9 +17,17 @@ class Baseboard(tk.Tk):
         self.attributes("-fullscreen", True)
         self.attributes("-type", "splash")
         self.current_frame = None
-        asyncio.run(self.updater())
+        
+        self.start_updater_watcher()
+        
         self.show_screen(WelcomeScreen)
         # self.show_screen(PairCardPage)
+        
+    def start_updater_watcher(self):
+        # Create and start a thread to run the general_scan main function
+        thread = threading.Thread(target=asyncio.run, args=(self.updater(),))
+        thread.daemon = True  # This ensures the thread will close when the main program exits
+        thread.start()
 
     def show_screen(self, screen_class, *args, **kwargs):
         new_frame = screen_class(self, *args, **kwargs)
@@ -36,7 +46,6 @@ class Baseboard(tk.Tk):
     async def updater(self):
         async with websockets.connect("ws://73.157.88.153:8000/wss") as websocket:
             print("HI WSS")
-            
             try:
                 commit_hash = os.popen('git rev-parse HEAD').read().strip()
                 data = {
@@ -45,11 +54,21 @@ class Baseboard(tk.Tk):
                 }
                 await websocket.send(json.dumps(data))
                 print("DONE!!!")
+                await self.receive_messages(websocket)
             except Exception as e:
                 print(f"Error sending confirmation: {e}")
+        
+        print("BYE WSS -- RETURNING")
         return
 
-
+    async def receive_messages(self, websocket):
+        try:
+            async for message in websocket:
+                print(f"Received message: {message}")
+                # Handle the received message here
+                # For example, you can update the UI or process the data
+        except websockets.ConnectionClosed:
+            print("WebSocket connection closed")
 
 if __name__ == "__main__":
     app = Baseboard()
